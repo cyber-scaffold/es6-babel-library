7174c24cff7552cce9a12f4b192e5aea4e142331
yushicheng <1542874601@qq.com>
2021-01-05 13:34:03 +0800

    temp:临时节点标记 星期二 下午 1:34:02 一月 5日 2021 修改了 .babelrc.js 修改了 package.json 修改了 src/index.js 删除了 src/index-style-scss.scss 删除了 src/test/index-style-scss.scss 删除了 src/test/index.js

--- src/test/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classnames from "classnames";
-
-import css from "./index-style-scss.scss";
-
-export default function FunctionComponent() {
-  return <div className={classnames(css.container)}>wellcome to use cyber-es6-lib</div>
-}
