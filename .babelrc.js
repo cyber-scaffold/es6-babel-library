29df3c48997880f0ffeadc9fd21b547d5b9aca3d
yushicheng <1542874601@qq.com>
2021-03-25 12:40:56 +0800

    temp:临时节点标记 星期四 下午 12:40:56 三月 25日 2021 修改了 .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,14 @@
 /* eslint-disable */
-const deepExtend=require("deep-extend");
 const basic_babel_config=require("@cyber-tools/preset-babel-option");
 
-module.exports = deepExtend(basic_babel_config,{
-  plugins:[
-    [require.resolve("babel-plugin-module-resolver"),{
-      root: ["./src/"],
-      alias: {
-        "@": "./src/",
-        "@@":"./"
-      }
-    }]
-  ]
-});
+basic_babel_config.plugins.push([
+  require.resolve("babel-plugin-module-resolver"),{
+    root: ["./src/"],
+    alias: {
+      "@": "./src/",
+      "@@":"./"
+    }
+  }
+]);
+
+module.exports=basic_babel_config;
