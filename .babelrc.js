0bed2eff14319a97ca2d858484d1c237e0888bbb
yushicheng <1542874601@qq.com>
2021-04-23 19:27:33 +0800

    temp:临时节点标记 星期五 晚上 7:27:33 四月 23日 2021 修改了 .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
+const path=require("path");
 const basic_babel_config=require("@cyber-tools/preset-babel-option");
 
 basic_babel_config.plugins.push([
   require.resolve("babel-plugin-module-resolver"),{
-    root: ["./src/"],
+    baseUrl:__dirname,
+    root: [path.resolve(__dirname,"./src/")],
     alias: {
       "@": "./src/",
       "@@":"./"
